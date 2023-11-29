e059bca555a21101aaf5d43f48a69e63c37ee5ca
bumobo <mobo@ukr.net>
2023-11-29 12:25:13 +0200

    recursion

---
+++ test.py
@@ -0,0 +1,13 @@
+image = [] #all_files[0]
+video = [] #all_files[1]
+document = [] #all_files[2]
+music = [] #all_files[3]
+archive = [] #all_files[4]
+other = []
+
+result_sort = [image, video, document, music, archive, other]
+
+result_sort[1].append('1')
+video.append('2')
+
+print(result_sort)
